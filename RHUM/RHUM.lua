272421be03b9ff7d29889351f577cc5234b5a7d1
Fuefib <fuefib@notanemail.com>
2014-06-08 18:17:00 +0200

    Finishing main stuff

--- RHUM/RHUM.lua
+++ RHUM/RHUM.lua
@@ -2,14 +2,20 @@ local libMsg = LibStub:GetLibrary("libMsg")
 
 local RHUM = {}
 
+-- Constants
+RHUM.ultiSlotNum = 8
+RHUM.delimeter = "_"
+RHUM.separator = " ;; "
+RHUM.regexp = RHUM.delimeter .. "(.+)" .. RHUM.delimeter .. RHUM.separator .. RHUM.delimeter .. "(.+)" .. RHUM.delimeter
+
 -- Status
 RHUM.isMember = false
 RHUM.isLeader = false
 RHUM.isUltiReady = false
 
 RHUM.leaderName = ""
+RHUM.membersStatus = {}
 
-RHUM.isLogDebug = true -- Set at 'true' to show debug informations
 
 -- Addon ids
 RHUM.slashCommand = "rhum"
@@ -24,29 +30,46 @@ RHUM.debugParam = "debug"
 RHUM.deltaTime = 3
 RHUM.previousTime = GetTimeStamp()
 
+-- Debug
+RHUM.isLogDebug = false -- Set at 'true' to show debug informations
+
 -- -------------------------
 -- Slash Commands handler
 -- -------------------------
 
 -- Activate RHUM as Leader
 SLASH_COMMANDS["/" .. RHUM.slashCommand .. RHUM.leaderParam] = function (param)
-	RHUM.debug("Activating RHUM as Leader")
+	RHUM.info("Activating RHUM as Leader")
 	RHUM.isLeader = true
 end
 
 -- Activate RHUM as Member
 SLASH_COMMANDS["/" .. RHUM.slashCommand .. RHUM.memberParam] = function (param)
-	RHUM.debug("Activating RHUM as Member")
+	RHUM.info("Activating RHUM as Member")
 	RHUM.isMember = true
 	RHUM.leaderName = param
 end
 
+-- Toggle RHUM debug
+SLASH_COMMANDS["/" .. RHUM.slashCommand .. RHUM.debugParam] = function (param)
+	if(RHUM.isLogDebug) then
+		RHUM.info("Deactivating debug")
+		RHUM.isLogDebug = false
+	else 
+		RHUM.info("Activating debug")
+		RHUM.isLogDebug = true
+	end
+end
+
 -- Shutdown RHUM
 SLASH_COMMANDS["/" .. RHUM.slashCommand] = function (param)
-	RHUM.debug("Shutting RHUM down")
+	RHUM.info("Shutting RHUM down")
 	RHUM.isMember = false
 	RHUM.isLeader = false
 	RHUM.isUltiReady = false
+	
+	RHUM.leaderName = ""
+	RHUM.membersStatus = {}
 end
 
 -- -------------------------
@@ -76,12 +99,10 @@ RHUM.updateMember = function ()
 	
 	if(ultiUp and not RHUM.isUltiReady) then
 		RHUM.debug("Ulti is now READY !")
-		RHUM.debug("sending message to " .. RHUM.leaderName)
-		--libMsg.sendMsg(RHUM.uniqueId, RHUM.leaderName, RHUM.uniqueId)
+		RHUM.buildAndSendMessage(ultiUp)
 	elseif (not ultiUp and RHUM.isUltiReady) then
 		RHUM.debug("Ulti is no longer ready :(")
-		RHUM.debug("sending message to " .. RHUM.leaderName)
-		--libMsg.sendMsg(RHUM.uniqueId, RHUM.leaderName, RHUM.uniqueId)
+		RHUM.buildAndSendMessage(ultiUp)
 	end
 	
 	RHUM.isUltiReady = ultiUp
@@ -90,11 +111,34 @@ end
 -- Update as Leader
 RHUM.updateLeader = function ()
 	RHUM.checkMessages()
+	RHUM.clearOffline()
+	RHUM.updateFrame()
+end
+
+-- Update frame
+RHUM.updateFrame = function ()
+
+	local membersText = ""
+
+	for name,ulti in pairs(RHUM.membersStatus) do 
+		membersText = membersText .. "[" .. name .. "]" .. " - " .. ulti .. "\n"
+	end
+	
+	RHUMMembers:SetText(membersText)
 end
 
 -- -------------------------
 -- Other Functions
 -- -------------------------
+
+-- Clear Offline : remove entries for offline members
+RHUM.clearOffline = function ()
+	for name,_ in pairs(RHUM.membersStatus) do 
+		if(not IsUnitOnline(name)) then
+			RHUM.membersStatus[name] = nil
+		end
+	end
+end
 
 -- Check Messages
 RHUM.checkMessages = function () 
@@ -102,22 +146,54 @@ RHUM.checkMessages = function ()
     local messages = libMsg.receiveAllMsg (RHUM.uniqueId)
 	local numReq = #messages
     RHUM.debug(tostring(numReq) .. " message(s) pending")
-	for key,value in pairs(messages) do 
-		RHUM.debug(key .. " sent " .. value)
+	for name,message in pairs(messages) do 
+		RHUM.debug(name .. " sent " .. message)
+		RHUM.handleMessage(name, message)
+	end
+end
+
+-- Message handler : update the status of members
+RHUM.handleMessage = function (name, message)
+	local ulti , status = string.match(message, RHUM.regexp)
+	
+	if(status == "true") then
+		RHUM.debug(name .. "'s ultimate [" .. ulti .. "] is UP ! (" .. status .. ")")
+		RHUM.membersStatus[name] = ulti
+	else
+		RHUM.debug(name .. "'s ultimate [" .. ulti .. "] no longer up (" .. status .. ")")
+		RHUM.membersStatus[name] = nil
 	end
 end
 
 -- Check Ultimate Status
 RHUM.checkUltimate = function ()
 	local current = GetUnitPower("player", POWERTYPE_ULTIMATE)
-	local required = GetSlotAbilityCost(8)
+	local required = GetSlotAbilityCost(RHUM.ultiSlotNum)
 	
 	return current >= required
 end
 
+
+-- Creating and sending message
+RHUM.buildAndSendMessage = function (ultiUp)
+	local ultiName = GetSlotName(RHUM.ultiSlotNum)
+
+	local message = RHUM.delimeter .. ultiName .. RHUM.delimeter 
+	message = message .. RHUM.separator 
+	message = message .. RHUM.delimeter .. tostring(ultiUp) .. RHUM.delimeter
+	
+	RHUM.debug("sending message >" .. message .. "< to " .. RHUM.leaderName)
+	libMsg.sendMsg(RHUM.uniqueId, RHUM.leaderName, message)	
+end
+
+
 -- -------------------------
--- Debug writting
+-- Logging
 -- -------------------------
+RHUM.info = function (message)
+	d("[RHUM] " .. message)
+end
+
 RHUM.debug = function (message)
 	if(RHUM.isLogDebug) then
 		d("[RHUM] debug -- " .. message)
